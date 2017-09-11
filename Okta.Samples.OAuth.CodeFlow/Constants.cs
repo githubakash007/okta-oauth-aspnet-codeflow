c303607d56883fa37de110f6257299e986c9fc88
Akash Agarwal <akash.software.1982@gmail.com>
2017-09-11 07:18:20 -0400

    initial commit hai

---
+++ Okta.Samples.OAuth.CodeFlow/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Okta.Samples.OAuth.CodeFlow
+{
+    internal class Constants
+    {
+        internal const string TokenEndpoint = "/oauth2/v1/token";
+        internal const string UserInfoEndpoint = "/oauth2/v1/userinfo";
+    }
+}
